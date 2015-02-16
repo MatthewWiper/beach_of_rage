608659def55e4cf6a15a853cc6a557ec1586d9c9
MatthewWiper <w0281488@nscc.ca>
2015-02-16 18:42:42 -0400

    initial work on economy

---
+++ gold.java
@@ -0,0 +1,13 @@
+package assignment_3;
+
+public class gold{
+  // currency drops/random finds. Used to buy equipment from shop
+  // define public int to count currency
+  // work shop into this class
+
+  public int currency = 0;
+
+
+
+
+}
