e34c3bf0fe7adfb5fa17dbaa11dc9332d066b49f
unknown <w0281488@INWSD21214.CAMPUS.NSCC.CA>
2015-02-20 13:29:52 -0400

    Initial combat prototyping. Random generators in place

---
+++ Combat.java
@@ -0,0 +1,28 @@
+package assignment_3;
+import java.util.random;
+public class Combat extends Enemy
+{
+int areaSeed = 0;
+
+// set seed value
+Enemy enemy = new Enemy();
+
+public int randHP(areaSeed)
+{
+}
+public int randArmour(areaSeed)
+{
+}
+public double randEvasion(areaSeed)
+{
+}
+/* if combat roll = success then call this class to initiate combat */
+
+
+
+
+
+
+}
+
+
